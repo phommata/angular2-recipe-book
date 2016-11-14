d6416a4b2c346d7176655d2b4d1e8dd967a68121
Andrew Phommathep <13destinies@gmail.com>
2016-11-13 18:28:03 -0800

    recipe-item
    cd src/app/recipe-list
    ng g c recipe-item --flat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
