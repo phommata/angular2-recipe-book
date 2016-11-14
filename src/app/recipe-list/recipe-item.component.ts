670ac7b64d10c9602ad562a78531f99e30fd14d9
Andrew Phommathep <13destinies@gmail.com>
2016-11-13 18:34:47 -0800

    recipe-item & class model recipe
    cd src/app/recipe-list
    ng g c recipe-item --flat
    cd ..
    ng g cl recipe

--- src/app/recipe-list/recipe-item.component.ts
+++ src/app/recipe-list/recipe-item.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'rb-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
+  recipe;
+  recipeId;
+  
   constructor() { }
 
   ngOnInit() {
