3e9d3445af566d0ee3df14fe3f73e29d105451bf
Andrew Phommathep <13destinies@gmail.com>
2016-11-13 18:22:34 -0800

    Remove spec and css, add rb-recipes

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'rb-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
 
