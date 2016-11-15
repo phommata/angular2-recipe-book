c08af7b93636675e77a2024c4145c3ef007f11cd
Andrew Phommathep <13destinies@gmail.com>
2016-11-14 23:37:25 -0800

    rb-shopping-list

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html',
-  styleUrls: ['./shopping-list-add.component.css']
 })
 export class ShoppingListAddComponent implements OnInit {
 
