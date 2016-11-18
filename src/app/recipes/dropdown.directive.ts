f6c2e2ecf2599be3aa86334d8fa7a0b64593625e
Andrew Phommathep <13destinies@gmail.com>
2016-11-17 23:38:35 -0800

    HostBinding, HostListener, rbDropdown

--- src/app/recipes/dropdown.directive.ts
+++ src/app/recipes/dropdown.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[rbDropdown]'
 })
 export class DropdownDirective {
-
-  constructor() { }
-
+  @HostBinding('class.open') get opened() {
+    return this.isOpen;
+  }
+  @HostListener('click') open() {
+    this.isOpen = true;
+  }
+  @HostListener('mouseleave') close() {
+    this.isOpen = false;
+  }
+  private isOpen = false;
 }
