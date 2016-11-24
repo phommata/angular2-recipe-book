bda1ad7634d7ea51e6626f4b8bc1d27a489955c4
Andrew Phommathep <13destinies@gmail.com>
2016-11-23 17:49:15 -0800

    RecipeService

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Recipe} from "./recipe";
 
 @Injectable()
 export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Wiener-Schnitzel02.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg', []),
+
+  ];
 
   constructor() { }
+
+  getRecipes(){
+    return this.recipes;
+  }
 
 }
