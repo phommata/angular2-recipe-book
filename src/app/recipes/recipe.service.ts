b5387f8fc9b5357e331feb7d1bd1aab59bc24136
Andrew Phommathep <13destinies@gmail.com>
2016-11-23 23:55:42 -0800

    Shopping list items

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+
 import {Recipe} from "./recipe";
+import {Ingredient} from "../shared/ingredient";
 
 @Injectable()
 export class RecipeService {
-  recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Wiener-Schnitzel02.jpg', []),
+  private recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Wiener-Schnitzel02.jpg', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Meat', 1),
+    ]),
     new Recipe('Summer Salad', 'Okayish', 'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg', []),
 
   ];
