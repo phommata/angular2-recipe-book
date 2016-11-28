dc4deb67950f35e8eec107259c5112e77e97bee0
Andrew Phommathep <13destinies@gmail.com>
2016-11-27 21:58:01 -0800

    100 Finishing Touches and some Cleanup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from "../recipe";
 import { RecipeService } from "../recipe.service";
@@ -16,7 +16,4 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onSelected(recipe: Recipe) {
-
-  }
 }
