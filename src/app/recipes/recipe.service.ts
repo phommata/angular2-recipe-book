f2bad66d738072d1ef87119d24aaec92fde3ca49
Andrew Phommathep <13destinies@gmail.com>
2016-11-27 21:49:31 -0800

    099 Wiring Edit and Delete up

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -23,4 +23,8 @@ export class RecipeService {
   getRecipe(id: number){
     return this.recipes[id];
   }
+
+  deleteRecipe(recipe: Recipe){
+    this.recipes.splice(this.recipes.indexOf(recipe), 1);
+  }
 }
