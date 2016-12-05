9cdfbf04d6d5eec742bd00a403562d3ac85422a4
Andrew Phommathep <13destinies@gmail.com>
2016-12-05 14:38:49 -0800

    189 Creating a Feature Module for Child Routes
    routing is not distributing to the rest of the recipe module application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,36 +5,25 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { DropdownDirective } from './recipes/dropdown.directive';
 import { RecipeService } from "./recipes/recipe.service";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { routing } from "./app.routing";
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start.component';
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
+import { RecipesModule } from "./recipes/recipe.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
     DropdownDirective,
-    RecipeEditComponent,
-    RecipeStartComponent,
   ],
   imports: [
     BrowserModule,
     HttpModule,
     routing,
-    ReactiveFormsModule,
-    ShoppingListModule
+    ShoppingListModule,
+    RecipesModule
   ],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
